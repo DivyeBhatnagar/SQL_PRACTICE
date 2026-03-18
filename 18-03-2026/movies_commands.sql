ff105469e9e12bc42a1adbdd8fca3b2e26a1ab60
DivyeBhatnagar <divyebhatnagar784@gmail.com>
2026-03-18 12:15:26 +0530

    4rd Task Done

--- 18-03-2026/movies_commands.sql
+++ 18-03-2026/movies_commands.sql
@@ -46,3 +46,22 @@ WHERE director != 'John Lasseter';
 SELECT *
 FROM movies
 WHERE title LIKE 'WALL-%';
+
+SELECT DISTINCT director
+FROM movies
+ORDER BY director ASC;
+
+SELECT *
+FROM movies
+ORDER BY year DESC
+LIMIT 4;
+
+SELECT *
+FROM movies
+ORDER BY title ASC
+LIMIT 5;
+
+SELECT *
+FROM movies
+ORDER BY title ASC
+LIMIT 5 OFFSET 5;
