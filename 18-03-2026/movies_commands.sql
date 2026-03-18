d69868ec67bb9b458596d82199867d5fdf96583f
DivyeBhatnagar <divyebhatnagar784@gmail.com>
2026-03-18 12:03:13 +0530

    Add more SQL queries for movies table

--- 18-03-2026/movies_commands.sql
+++ 18-03-2026/movies_commands.sql
@@ -14,3 +14,19 @@ FROM movies;
 
 SELECT *
 FROM movies;
+
+SELECT *
+FROM movies
+WHERE id = 6;
+
+SELECT *
+FROM movies
+WHERE year BETWEEN 2000 AND 2010;
+
+SELECT *
+FROM movies
+WHERE year NOT BETWEEN 2000 AND 2010;
+
+SELECT title, year
+FROM movies
+WHERE id <= 5;
