74d62511ae388237b8b248cb6dde62ad963f5da6
DivyeBhatnagar <divyebhatnagar784@gmail.com>
2026-03-18 12:18:52 +0530

    5rd Task Done

--- 18-03-2026/movies_commands.sql
+++ 18-03-2026/movies_commands.sql
@@ -65,3 +65,29 @@ SELECT *
 FROM movies
 ORDER BY title ASC
 LIMIT 5 OFFSET 5;
+
+SELECT city, population
+FROM north_american_cities
+WHERE country = 'Canada';
+
+SELECT *
+FROM north_american_cities
+WHERE country = 'United States'
+ORDER BY latitude DESC;
+
+SELECT *
+FROM north_american_cities
+WHERE longitude < -87.6298
+ORDER BY longitude ASC;
+
+SELECT *
+FROM north_american_cities
+WHERE country = 'Mexico'
+ORDER BY population DESC
+LIMIT 2;
+
+SELECT city, population
+FROM north_american_cities
+WHERE country = 'United States'
+ORDER BY population DESC
+LIMIT 2 OFFSET 2;
